caac64d9df916c9bae733b4b7516ec4f2f99b3d0
TerusLimsurut <few2012few@gmail.com>
2017-01-25 10:07:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,7 +2,8 @@
 //$ composer require linecorp/line-bot-sdk
 // $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('3NSrwxoBmoc/JzYfi/TeeAWDfjMXPkl+pK2smX+/wlptcnGgM/ysws0jfUfuaXInCd8/tPGW4MhzFYTyXlGB/8Ue8p8irgrbaXnFk8dz6vGieKqDaPgzPfI2SgrjG7f+dJ9+J+sbGISzY+GGSa07gwdB04t89/1O/w1cDnyilFU=');
 // $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'd47d8ecbdda055d3e577e5bc0cd7db97']);
-
+// Get POST body content
+$content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {

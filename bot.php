deba5c09a9b1c34d8403e309ff55e5cdb3008695
TerusLimsurut <few2012few@gmail.com>
2017-01-25 09:48:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$composer require linecorp/line-bot-sdk
+$ composer require linecorp/line-bot-sdk
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('3NSrwxoBmoc/JzYfi/TeeAWDfjMXPkl+pK2smX+/wlptcnGgM/ysws0jfUfuaXInCd8/tPGW4MhzFYTyXlGB/8Ue8p8irgrbaXnFk8dz6vGieKqDaPgzPfI2SgrjG7f+dJ9+J+sbGISzY+GGSa07gwdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'd47d8ecbdda055d3e577e5bc0cd7db97']);
 $response = $bot->replyText('<reply token>', 'hello!');
